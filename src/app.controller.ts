f79b80d5cd46922386612971a9aaf5931e52618d
Nagata Ichiko <106644839+nagata-ichiko@users.noreply.github.com>
2023-06-13 23:24:18 +0900

    ルーティングお試し

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('main')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hallo')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('by')
+  getby(): string {
+    return this.appService.getby();
   }
 }
