f73b5aa8120562431e5bf25df6b97b7c199ecb80
nagata-ichiko <eiichiro.kinoshita@i-standard.jp>
2023-06-14 10:38:42 +0900

    マイグレーション

---
+++ prisma/migrations/20230614013818_init/migration.sql
@@ -0,0 +1,53 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `email` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "email" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "name" SET DATA TYPE VARCHAR(255);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "bio" TEXT,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "my_user" (
+    "user_id" SERIAL NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT NOT NULL,
+    "tel" TEXT,
+
+    CONSTRAINT "my_user_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "my_user_last_name_key" ON "my_user"("last_name");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
